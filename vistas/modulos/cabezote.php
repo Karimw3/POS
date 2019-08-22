5beed3823172109e02607e009d0a205fcdb5b927
Karim Mayén <39173999+Karimw3@users.noreply.github.com>
2019-08-22 14:43:45 -0500

    Agregando modulos parte 2

---
+++ vistas/modulos/cabezote.php
@@ -0,0 +1,65 @@
+<header class="main-header">
+
+
+
+	<!-- Logotipo -->
+	<a href=""   class="logo">
+
+
+		<!-- Logo mini -->
+		<span class="logo-mini">
+
+			<img src="vistas/img/plantilla/icono-blanco.png" class="img-responsive" style="padding:2px">
+		</span>
+
+
+		<!-- Logo normal -->
+		<span class="logo-lg">
+			<img src="vistas/img/plantilla/icono-blanco-lineal.png" class="img-responsive" style="padding:0px 0px">
+		</span>
+
+	</a>
+
+
+
+	<!-- Sidebar o barra de navegación -->
+	<nav class="navbar navbar-static-top" role="navigation">
+
+		<!-- Botón de navegación -->
+		<a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+
+				<span class="sr-only">Toggle navigation</span>
+		</a>
+
+		<!-- Perfil de usuario -->
+		<div class = "navbar-custom-menu">
+
+			<ul class = "nav navbar-nav">
+
+				<li class = "dropdown user user-menu">
+					
+					<a href="#" class = "dropdown-toggle" data-toggle = "dropdown">
+						<img src="vistas/img/usuarios/default/anonymous.png" class = "user-image">
+						<span class ="hideen-xs">Usuario Genérico</span>
+					</a>
+
+					<!-- Dropdown-toggle -->
+					<ul class = "dropdown-menu">
+						<li class = "user-body">
+							<div class = "pull-right">
+								<a href="" class = "btn btn-default btn-flat">Salir</a>
+							</div>
+						</li>
+					</ul>
+					
+				</li>
+			</ul>
+
+		</div>
+
+	</nav>
+
+
+
+	
+</header>
